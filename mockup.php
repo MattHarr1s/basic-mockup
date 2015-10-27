6532b9491df652912011b7ca650b2e9642abac65
Matt Harris <mateodelay@gmail.com>
2015-10-27 15:43:35 -0600

    Made body into .container class

--- mockup.php
+++ mockup.php
@@ -5,7 +5,7 @@
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<title>Dylan's Profe$$ional Web Page</title>
 	</head>
-	<body>
+	<body class="container">
 		<!-headline for Dylan's professional web page->
 		<h1 class="headline">Dylan's Profe$$ional Web Page</h1>
 		<!-list of instructions that will go in a box->
