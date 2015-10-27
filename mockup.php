32b152d181393e9f90031dd484445dd4b8cc073c
Matt Harris <mateodelay@gmail.com>
2015-10-27 15:53:08 -0600

    Added ordered list

--- mockup.php
+++ mockup.php
@@ -9,5 +9,13 @@
 		<!-headline for Dylan's professional web page->
 		<h1 class="headline">Dylan's Profe$$ional Web Page</h1>
 		<!-list of instructions that will go in a box->
+		<ol>
+		Instructions
+			<li> Fork and clone the following GitHub repository, and create a new project in PHPStorm:
+				https://github.com/rlewis2892/basic-mockup</li>
+			<li>Using Basic CSS, style this page like the mock up image here: <a href="img/mockup.jpg">Mockup Image</a>
+			</li>
+			<li> Have fun! We will be going over each student's project in class .:D</li>
+		</ol>
 		</body>
 </html>
